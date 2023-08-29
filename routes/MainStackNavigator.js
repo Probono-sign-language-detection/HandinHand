eafd6844a3027f1aac83a9586b010594bcc9e42d
kye1115z <kye1115z@naver.com>
2023-08-29 19:31:44 +0900

    :memo: docs: Home.js 추가

--- routes/MainStackNavigator.js
+++ routes/MainStackNavigator.js
@@ -5,16 +5,18 @@ import Login from "../screens/Login";
 import Signup from "../screens/Signup"
 import Start from "../screens/Start";
 import RealTimeImg from '../screens/Camera';
+import Home from "../screens/Home";
 
 const Stack = createStackNavigator();
 
 function MainStackNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Start">
+            <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Start" component={Start} options={{headerShown: false}} />
                 <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
                 <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}} />
+                <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
                 <Stack.Screen name="RealTimeImg" component={RealTimeImg} options={{headerShown: false}} />
             </Stack.Navigator>
         </NavigationContainer>
